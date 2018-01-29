e4ed39d9b42bb355d4725ee2289cf9cc764debad
YumeMichi <do4suki@gmail.com>
2018-01-29 18:42:27 +0800

    bacon: Switch to XML audio policy configuration
    * Based on https://github.com/LineageOS/android_device_htc_msm8974-common/commit/3f841945a765199ddaa1acc2492ab88fc9b613eb
    * Remove deprecated incall music and multichannel policy.
    
    Change-Id: I8d3e241fd16edba9c0c17ffa574f5a86ea207517
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -49,7 +49,17 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml 
+
+    PRODUCT_COPY_FILES += \
+       frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+       frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+       frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+       frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+       frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    
+    
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
