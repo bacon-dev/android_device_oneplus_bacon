f49e325adeed81625641bd897b9b5a986b224077
Alessandro Astone <ales.astone@gmail.com>
2019-03-02 19:21:11 +0100

    bacon: camera: Set sdk version for blobs that need textrel
    Change-Id: I4c944a2ae9b49767d7c47b6b31e5f697ab44b917

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 # Camera
 TARGET_USES_MEDIA_EXTENSIONS := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /system/vendor/bin/mm-qcamera-daemon=23
+    /system/vendor/bin/mm-qcamera-daemon=23 \
+    /system/vendor/bin/cameraserver=23 \
+    /system/vendor/bin/mediaserver=23 \
 APPLY_MSM8974_1440P_EGL_WORKAROUND := true
 HAS_LEGACY_CAMERA_HAL1 := true
 
