815a0303e5df83366667079673fdd41f7cac349a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-13 16:41:34 -0300

    bacon: Use alternate manual network selection implementation
    * Enable TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT

--- bacon.mk
+++ bacon.mk
@@ -249,6 +249,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Telephony
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
