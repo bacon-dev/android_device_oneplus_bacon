993d563b142643dbc4aea01a296cb91b19717252
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    bacon: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -283,6 +283,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8974.conf
