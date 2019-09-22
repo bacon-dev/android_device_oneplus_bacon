9f11a8e49b62886062c660831613aa10bdbddb9f
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    bacon: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- bacon.mk
+++ bacon.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Encryption
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
