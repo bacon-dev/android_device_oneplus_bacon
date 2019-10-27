e4e7fcfed1f37fb7bc9d290c76e9cc737bb3f1b0
erfanoabdi <erfangplus@gmail.com>
2019-10-27 18:44:19 +0000

    BoardConfig: set loop.part.max
    Change-Id: I72746d59efedf3561f9c79e80929d7aa0198c7ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,7 @@ TARGET_KERNEL_ARCH := arm
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.btmacaddr=00:00:00:00:00:00
 TARGET_KERNEL_CONFIG := fz_bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
